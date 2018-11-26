b1442a152bfe156ef64506ab6707685869a54ecb
Nathaniel Ott Homer <nathaniel.ott.homer@gmail.com>
2018-11-26 17:01:29 -0500

    reorganize routes to properly catch redirects

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
+  get '/:short_url', to: 'api/links#re_direct', as: 're_direct'
+
   namespace :api do
     resources :links
 
     get '/top', to: 'links#top', as: 'top'
   end
-
-  
-  get '/:short_url', to: 'links#re_direct', as: 're_direct'
 end
