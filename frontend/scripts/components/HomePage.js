388993133834caca4223a18230a616d13f494762
Nathaniel Ott Homer <nathaniel.ott.homer@gmail.com>
2018-12-03 15:11:16 -0500

    add copy to clipboard functionality

--- frontend/scripts/components/HomePage.js
+++ frontend/scripts/components/HomePage.js
@@ -15,6 +15,7 @@ export default class HomePage extends React.Component {
     this.handleURLChange = this.handleURLChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
     this.renderShortLink = this.renderShortLink.bind(this)
+    this.handleClickToCopy = this.handleClickToCopy.bind(this)
   }
 
   handleURLChange (e) {
@@ -34,16 +35,27 @@ export default class HomePage extends React.Component {
       this.setState({ long_url: '', short_url })
     })
   }
+
+  handleClickToCopy (e) {
+    this.textarea.select()
+    document.execCommand('copy')
+  }
   
   renderShortLink () {
     if (this.state.short_url.length > 0) {
+      const fullURL = getLocation() + this.state.short_url
       return (
         <div className="short-link">
-          <span>Your new shortlink is: {getLocation() + this.state.short_url}</span>
-          <span className="click-to-copy" 
+          <span>Your new shortlink is: {fullURL}</span>
+          <span className="click-to-copy" id="click-to-copy"
             onClick={this.handleClickToCopy}>
             click here to copy to clipboard
           </span>
+          <textarea 
+            readOnly 
+            value={fullURL} 
+            ref={textarea => {this.textarea = textarea}}
+            className="hidden-input" />
         </div>
       )
       
